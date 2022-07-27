5f5588bd0e8cce10c3796a9d97bfb13962ba1002
Lygia Bueno <lybueno@gmailo.com>
2022-07-27 16:54:19 -0300

    Local project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.java.workshopjavafxjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.java.workshopjavafxjdbc to javafx.fxml;
+    exports com.java.workshopjavafxjdbc;
+}
