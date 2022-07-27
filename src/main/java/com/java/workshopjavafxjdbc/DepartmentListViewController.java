a04552b42f282ffadce0e9e7d7ed896e9cabb08d
Lygia Bueno <lybueno@gmailo.com>
2022-07-27 18:49:30 -0300

    DepartmentList controller

--- src/main/java/com/java/workshopjavafxjdbc/DepartmentListViewController.java
+++ src/main/java/com/java/workshopjavafxjdbc/DepartmentListViewController.java
@@ -1,4 +1,46 @@
 package com.java.workshopjavafxjdbc;
 
-public class DepartmentListViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentListViewController implements Initializable {
+
+    @FXML
+    private TableView<Department> tableViewDepartment;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button btNew;
+
+    @FXML
+    public void onBtNewAction(){
+        System.out.println("onBtNewAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
 }
