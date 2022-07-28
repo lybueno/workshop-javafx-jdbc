d42ea147fa3a4aa51d997262044dc1dfc7f720f8
Lygia Bueno <lybueno@gmailo.com>
2022-07-28 10:56:11 -0300

    Adding database access

---
+++ src/main/java/db/DbExecption.java
@@ -0,0 +1,8 @@
+package db;
+
+public class DbExecption extends RuntimeException{
+
+    public DbExecption(String message) {
+        super(message);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.java.workshopjavafxjdbc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.java.workshopjavafxjdbc to javafx.fxml;
